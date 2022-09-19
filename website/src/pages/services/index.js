2b8440b8857327939e04630ac1aa96c5e3ed4239
David Douglas Gonçalves Ribeiro <David Douglas Gonalves Ribeiro>
2022-09-19 11:54:26 -0300

    Header

---
+++ website/src/pages/services/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import StyledServices from './styles';
+
+export default function Services() {
+    return(
+    <StyledServices>
+      <label>
+
+      </label>
+    </StyledServices>
+
+
+  )
+
+
+
+
+};
